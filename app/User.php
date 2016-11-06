9d332791648f4e8181c9e3ba213cb82bcf271d77
Yasser Carreón <yacasia@gmail.com>
2016-11-06 17:29:02 -0600

    Included user relationships

--- app/User.php
+++ app/User.php
@@ -31,4 +31,9 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
 	 */
 	protected $hidden = ['password', 'remember_token'];
 
+	public function albums() 
+	{
+		return $this->hasMany('ImagesManager/Album');
+	}
+
 }
