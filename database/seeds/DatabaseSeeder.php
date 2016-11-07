b366a45eef5a9cbd1a90f472bd2d3906be967457
Yasser Carreón <yacasia@gmail.com>
2016-11-07 08:57:05 -0600

    Called the seeders from DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 
+use ImagesManager\User;
+use ImagesManager\Album;
+use ImagesManager\Photo;
+
 class DatabaseSeeder extends Seeder {
 
 	/**
@@ -12,9 +16,18 @@ class DatabaseSeeder extends Seeder {
 	 */
 	public function run()
 	{
-		Model::unguard();
+		// Model::unguard();
 
-		// $this->call('UserTableSeeder');
+		// Doesn't check foreign keys to allow truncation
+		DB::statement('SET FOREIGN_KEY_CHECK = 0');
+
+		Photo::truncate();
+		Album::truncate();
+		User::trucate();
+
+		$this->call('UsersTableSeeder');
+		$this->call('AlbumsTableSeeder');
+		$this->call('PhotosTableSeeder');
 	}
 
 }
